e513f033e32422b4fbe5930e5caa52c069441de8
Loic Pittet <lpittet@c2r5s2.42lausanne.ch>
2024-11-08 15:51:43 +0100

    no bonus zoom

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: lpittet <lpittet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 14:10:36 by lpittet           #+#    #+#             */
-/*   Updated: 2024/11/08 13:17:33 by lpittet          ###   ########.fr       */
+/*   Updated: 2024/11/08 15:10:02 by lpittet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	zoom(t_data *data, double factor)
 {
-	double	c_x;
-	double	c_y;
+	// double	c_x;
+	// double	c_y;
 	double	diff_x;
 	double	diff_y;
 
-	mlx_mouse_get_pos(data->mlx_win, &data->mouse_x, &data->mouse_y);
-	c_x = data->min_x + (data->mouse_x * (data->max_x - data->min_x) / WIDTH);
-	c_y = data->max_y - (data->mouse_y * (data->max_y - data->min_y) / HEIGTH);
+	// mlx_mouse_get_pos(data->mlx_win, &data->mouse_x, &data->mouse_y);
 	diff_x = (data->max_x - data->min_x) * factor / 2;
-	diff_y = diff_x * HEIGTH / WIDTH;
-	data->max_x = c_x + diff_x;
-	data->min_x = c_x - diff_x;
-	data->max_y = c_y + diff_y;
-	data->min_y = c_y - diff_y;
+	diff_y = (data->max_y - data->min_y) * factor / 2;
+	// c_x = 	data->min_x * factor + (data->mouse_x * (2 * diff_x) / WIDTH);
+	// c_y = data->max_y * factor - (data->mouse_y * (2 * diff_y) / HEIGTH);
+	data->max_x = diff_x;
+	data->min_x = -diff_x;
+	data->max_y = diff_y;
+	data->min_y = -diff_y;
 	render(data);
 }
